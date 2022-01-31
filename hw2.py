6f99a329484351a9135741848d16d9ae123a20d9
Anna_Kabanova <K&7WRt4!>
2022-01-31 22:59:01 +0200

    Added create_common_dictionary method to the HW2 branch

--- hw2.py
+++ hw2.py
@@ -3,17 +3,6 @@ from random import randint, choice  # import library for creating random list
 import string
 import collections
 
-# 1. create a list of random number of dicts (from 2 to 10)
-
-# dict's random numbers of keys should be letter,
-# dict's values should be a number (0-100),
-# example: [{'a': 5, 'b': 7, 'g': 11}, {'a': 3, 'c': 35, 'g': 42}]
-# 2. get previously generated list of dicts and create one common dict:
-
-# if dicts have same key, we will take max value, and rename key with dict number with max value
-# if key is only in one dict - take it as is,
-# example: {'a_1': 5, 'b': 7, 'c': 35, 'g_2': 42}
-# Each line of code should be commented with description.
 random_dict_list=[]
 
 def create_random_collection():
