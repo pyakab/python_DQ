c0970cf66af5812227a0ce9a39cc5486774e2431
Anna_Kabanova <K&7WRt4!>
2022-02-08 23:05:50 +0200

    create_two_new_methods

--- hw2.py
+++ hw2.py
@@ -1,12 +1,20 @@
-import random   # import random library
+import random  # import random library
 from random import randint, choice  # import library for creating random list
-import string   # import string library
+import string  # import string library
 import collections  # import collections library
 
 random_dict_list = []  # initialize list for the dictionary
+letters = ''  # initialized string of lowercase letters
+#random_dict = {}  # initialize the dictionary
+tested_dictionary = {}
+duplicates = []
+key_list = []
+#one_common_list = {}
+#clear_uniq = []  # initialize list for unique keys
+#duplicate_list = {}  # initialize the dictionary for the repeated key
 
 
-def create_random_collection():  # define the method for creating the list of 10 random dictionaries
+def create_random_collection(letters,random_dict_list): # define the method for creating the list of 10 random dictionaries
     letters = string.ascii_lowercase  # initialized string of lowercase letters
     for i in range(0, 10):  # set the conditions of the loop
         x = 0  # initialize the x variable, which will be used in the loop
@@ -17,25 +25,30 @@ def create_random_collection():  # define the method for creating the list of 10
             random_dict[letter_key] = n  # add item (key and value) to the dictionary
             x = x + 1  # increase the counter x
         random_dict_list.append(random_dict)  # add created dictionary to the list
-        # i = i+1  # increase the counter i
-    print(random_dict_list)  # print the generated list of dictionary
+
+    return random_dict_list
 
 
-def create_common_dictionary():  # define the method for creating the common dictionary
+def create_key_list(random_dict_list):  # define the method for creating the common dictionary
     i = 0  # initialize the i variable, which will be used in the loop
-    key_list = []  # initialize the list
-    # tested_dictionary={}  # initialize the dictionary
 
     while i < len(random_dict_list):  # set the conditions of the loop
         tested_dictionary = random_dict_list[i]  # set tested_dictionary = [i] dictionary from the random_dict_list
         for key in tested_dictionary:  # set the conditions of the loop
             key_list.append(key)  # add the key from the dictionary to the list of keys
-        i = i+1  # increase the counter i
-    # print(key_list)  # print the key_list in console
+        i = i + 1  # increase the counter i
+    return key_list
 
+def create_duplicates_list(key_list):
     duplicates = [item for item, count in collections.Counter(key_list).items() if count > 1]  # add to the duplicates list keys that are repeated in the key_list
-    # print(duplicates)  # print the duplicates list in console
+    print(duplicates)  # print the duplicates list in console
+    return duplicates
+
+
+def create_common_dictionary(key_list, duplicates,random_dict_list):
+    print(key_list)
     uniq = set(key_list)  # clear the key_list from duplicates
+    print(len(uniq))
 
     if len(duplicates) <= 0:  # the condition for checking if duplicates list is empty
         one_common_list = {}  # initialize list for the all keys in the random_dict_list
@@ -45,7 +58,8 @@ def create_common_dictionary():  # define the method for creating the common dic
             tested_dictionary = random_dict_list[y]  # set tested_dictionary = [y] dictionary from the random_dict_list
             for z in uniq:  # set the conditions of the loop
                 if z in tested_dictionary:  # the condition for checking if keys is in the [y] dictionary
-                    one_common_list[z] = tested_dictionary[z]  # add item (key and value) to the one_common_list dictionary
+                    one_common_list[z] = tested_dictionary[
+                        z]  # add item (key and value) to the one_common_list dictionary
 
             y = y + 1  # # increase the counter y
 
@@ -63,37 +77,49 @@ def create_common_dictionary():  # define the method for creating the common dic
             tested_dictionary = random_dict_list[y]  # set tested_dictionary = [y] dictionary from the random_dict_list
             for z in clear_uniq:  # set the condition of the loop
                 if z in tested_dictionary:  # the condition for checking if keys is in the [y] dictionary
-                    one_common_list[z] = tested_dictionary[z]  # add item (key and value) to the one_common_list dictionary
+                    one_common_list[z] = tested_dictionary[
+                        z]  # add item (key and value) to the one_common_list dictionary
 
-            y = y+1  # increase the counter y
+            y = y + 1  # increase the counter y
 
         for m in duplicates:  # set the condition of the loop
             duplicate_list = {}  # initialize the dictionary for the repeated key
             k = 0  # initialize the k variable, which will be used in the loop
 
             while k < len(random_dict_list):  # set the condition of the loop
-                if m in random_dict_list[k]:  # the condition for checking if key is in the k-dictionary in the random_dict_list
-                    tested_dictionary = random_dict_list[k]  # set tested_dictionary = [y] dictionary from the random_dict_list
-                    final_key_name = str(m)+"_"+str(k+1)  # set the final_key_name as concat m and (k+1) variables
-                    duplicate_list[final_key_name] = tested_dictionary[m]  # add item (key and value) to the one_common_list dictionary
+                if m in random_dict_list[
+                    k]:  # the condition for checking if key is in the k-dictionary in the random_dict_list
+                    tested_dictionary = random_dict_list[
+                        k]  # set tested_dictionary = [y] dictionary from the random_dict_list
+                    final_key_name = str(m) + "_" + str(k + 1)  # set the final_key_name as concat m and (k+1) variables
+                    duplicate_list[final_key_name] = tested_dictionary[
+                        m]  # add item (key and value) to the one_common_list dictionary
 
                 else:  # if condition is false
                     pass  # do nothing
 
-                k = k+1  # increase the counter k
+                k = k + 1  # increase the counter k
                 if k == len(random_dict_list):  # check if the k = the length of list
-                    biggest_key = max(duplicate_list, key=duplicate_list.get)  # get the key with the biggest value from the duplicate_list dictionary
+                    biggest_key = max(duplicate_list,
+                                      key=duplicate_list.get)  # get the key with the biggest value from the duplicate_list dictionary
                     # print(biggest_key)
-                    one_common_list[biggest_key] = duplicate_list[biggest_key]  # add item (key and value) to the one_common_list dictionary
+                    one_common_list[biggest_key] = duplicate_list[
+                        biggest_key]  # add item (key and value) to the one_common_list dictionary
                     # print(one_common_list)
                 else:  # if condition is false
                     pass  # do nothing
 
     if len(uniq) == len(one_common_list):  # check if the count of elements in the received list = count in the uniq
-        print(one_common_list)  # print the one_common_list in console
+        # print(one_common_list)  # print the one_common_list in console
+        return one_common_list
     else:  # if condition is false
-        print('Houston, we have a problem!')  # print the message about the error in console
+        # print('Houston, we have a problem!')  # print the message about the error in console
+        return 'Houston, we have a problem!'
 
 
-create_random_collection()  # call the method create_random_collection
-create_common_dictionary()  # call the method create_common_dictionary
+create_random_collection(letters,random_dict_list)  # call the method create_random_collection
+create_key_list(random_dict_list)
+create_duplicates_list(key_list)
+create_common_dictionary(duplicates, key_list, random_dict_list)  # call the method create_common_dictionary
+#print(create_random_collection(letters,random_dict_list))
+print(create_common_dictionary(duplicates, key_list, random_dict_list))
