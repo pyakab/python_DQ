21ee396305db15a4036dd5fca9848cacd67b2b85
Anna_Kabanova <K&7WRt4!>
2022-11-30 14:08:05 +0200

    Improving the method for creating CSV files

--- hw7_createCSV.py
+++ hw7_createCSV.py
@@ -1,21 +1,23 @@
 from datetime import date, datetime
 import os
+import os.path
 import csv
 
 current_date = datetime.today()
 
-class Publication():
+
+class Publication:
     def __init__(self, text):
         self.text = text
 
     def publishing(self):
         return f"{self.text}"
 
+
 class News(Publication):
     def __init__(self, text, city):
         Publication.__init__(self, text)
-        self.city=city
-
+        self.city = city
 
     def publishing_news(self):
         news_date = date.today()
@@ -28,11 +30,11 @@ class PrivateAd(Publication):
         Publication.__init__(self, text)
         self.expiration_date = expiration_date
 
-
     def publishing_ad(self):
         ndays = (self.expiration_date - current_date).days
         with open("news_feed.txt", "a") as text_file:
-            text_file.write(f"\n\nPrivate ad------------\n{self.text} \nActual until: {self.expiration_date}, {ndays} days left.")
+            text_file.write(
+                f"\n\nPrivate ad------------\n{self.text} \nActual until: {self.expiration_date}, {ndays} days left.")
 
 
 class BlogPost(Publication):
@@ -45,39 +47,84 @@ class BlogPost(Publication):
     def publishing_post(self):
         print(self.text)
         with open("news_feed.txt", "a") as text_file:
-            text_file.write(f"\n\nBlog post------------\n {self.title} \n Author: {self.author} \n {self.text} \n tag: {self.tag}")
+            text_file.write(
+                f"\n\nBlog post------------\n {self.title} \n Author: {self.author} \n {self.text} \n tag: {self.tag}")
+
 
 class FileUpload(Publication):
-    def __init__(self, filepath):
+    def __init__(self, filepath, text):
+        super().__init__(text)
         self.filepath = filepath
 
-
     def publishing_from_file(self):
-        with open(self.filepath, 'r') as f:
-            filetext = f.read()
-        print(filetext)
-        with open("news_feed.txt", "a") as text_file:
-            text_file.write(f"\n\n{filetext}")
+        with open(self.filepath) as f:
+            for line in f:
+                line = line.lower()
+                if line.startswith('news'):
+                    city = next(f).strip()
+                    print(city)
+                    if city != '':
+                        text = next(f).strip()
+                        print(text)
+                        new_news = News(text, city)
+                        new_news.publishing_news()
+                    else:
+                        print('File has the wrong format')
+                        break
 
+                if line.startswith('private ad'):
+                    deadline_date = next(f).strip()
+                    try:
+                        expiration_date = datetime.strptime(deadline_date, '%Y/%m/%d')
+                    except:
+                        print(
+                            'Wrong date format!\nPlease repeat the input of ad with the right date format(yyyy/mm/dd)')
+                        break
+                    if (expiration_date - current_date).days < 0:
+                        print(
+                            f'Wrong date was input - {deadline_date}\n'
+                            f'Please repeat the input of ad with the right date (later than today)')
+                        break
+                    if deadline_date != '':
+                        text = next(f).strip()
+                        new_private_ad = PrivateAd(text, expiration_date)
+                        new_private_ad.publishing_ad()
+                    else:
+                        print('File has the wrong format')
 
+            if line.startswith('blog post'):
+                title = next(f).strip()
+                if title != '':
+                    author = next(f).strip()
+                    if author != '':
+                        text = next(f).strip()
+                        if text != '':
+                            tag = next(f).strip()
+                            new_blogpost = BlogPost(text, title, author, tag)
+                            new_blogpost.publishing_post()
+                            upload_to_print = FileUpload(self.filepath)
+                            upload_to_print.delete_file()
+
+                else:
+                    print('Text have the wrong format')
 
     def delete_file(self):
-        file_for_delete=self.filepath
+        file_for_delete = self.filepath
         os.remove(file_for_delete)
 
-class Normalization():
+
+class Normalization:
 
     def __init__(self):
         self.text_file = 'news_feed.txt'
 
-
     def normalizing_file(self):
-        text_file=self.text_file
+        text_file = self.text_file
         with open(text_file, 'r') as f:
             text_file = f.read()
-        text_file=text_file.lower()
-        updated_file=[]
-        text_file1=[]
+        text_file = text_file.lower()
+        updated_file = []
+        text_file1 = []
         x = 0
         while x < len(text_file):
             if text_file[x] == " " and text_file[x + 1] == " ":
@@ -96,7 +143,7 @@ class Normalization():
         while y < len(updated_file):
             if updated_file[y - 1] == "\n":
                 text_file1.append(updated_file[y].upper())
-            elif updated_file[y - 2]=="\n" and updated_file[y - 1]==" " :
+            elif updated_file[y - 2] == "\n" and updated_file[y - 1] == " ":
                 text_file1.append(updated_file[y].upper())
             elif updated_file[y - 2] == "." and updated_file[y - 1] == " ":
                 text_file1.append(updated_file[y].upper())
@@ -117,19 +164,17 @@ class Normalization():
 
         s1 = ""
         final_formatted_text = s1.join(text_file1)
-        if (os.path.exists("normalized_newsfeed.txt")):
+        if os.path.exists("normalized_newsfeed.txt"):
             os.remove("normalized_newsfeed.txt")
         else:
             print('File isn`t found')
         with open("normalized_newsfeed.txt", "a") as text_file:
             text_file.write(f"{final_formatted_text}")
 
-        edited_file='normalized_newsfeed.txt'
-        return edited_file
 
-class FileCSV():
+class FileCSV:
     def __init__(self):
-        self.newsfeed="normalized_newsfeed.txt"
+        self.newsfeed = "normalized_newsfeed.txt"
 
     def count_words(self):
         text = open(self.newsfeed, "rt")
@@ -145,7 +190,7 @@ class FileCSV():
                 else:
                     d[word] = 1
 
-        if (os.path.exists("word_stat.csv")):
+        if os.path.exists("word_stat.csv"):
             os.remove("word_stat.csv")
         else:
             print('File isn`t found')
@@ -160,79 +205,95 @@ class FileCSV():
         lines = 0
         words = 0
         characters = 0
-        lower = 0
         upper = 0
         digits = 0
-        whitespace = 0
+
         for line in infile:
             wordslist = line.split()
             lines = lines + 1
             words = words + len(wordslist)
             characters += sum(len(word) for word in wordslist)
 
+        all_freq = {}
+
         with open("normalized_newsfeed.txt") as file:
-            #count = 0
             text = file.read()
+            text = text.lower()
             for i in text:
                 if i.isupper():
                     upper += 1
-                elif (i.isspace()):
-                    whitespace += 1
-                elif (i.islower()):
-                    lower += 1
                 elif (i.isdigit()):
                     digits += 1
 
-        print(f"The number of characters is {characters}.")
-        print(f"The number of uppercases is {upper}.")
-        print(f"The number of whitespaces is {whitespace}.")
+            text = ''.join([i for i in text if not i.isdigit()])
+            all_punct = '''!()-[]{};:'="`\,<>./?@#$%^&*_~'''
+
+            for elements in text:
+                if elements in all_punct:
+                    text = text.replace(elements, "")
+
+            text = "".join(text.split())
+
+            for i in text:
+                if i in all_freq:
+                    all_freq[i] += 1
+                else:
+                    all_freq[i] = 1
+
+        if os.path.exists("letters_stat.csv"):
+            os.remove("letters_stat.csv")
+        else:
+            print('File isn`t found')
+
+        with open('letters_stat.csv', 'w', newline='') as f:
+            writer = csv.writer(f)
+            for k, v in all_freq.items():
+                writer.writerow([k, v])
 
 
-class main():
+class Main:
     y = 0
-    while y==0 :
+    while y == 0:
         print('Do you want to add text from file or input it with keyboard? (Options: file / keyboard)')
         inputway = input()
         print(f'Your answer is:\n{inputway}')
-        if inputway.lower()=='keyboard':
+        if inputway.lower() == 'keyboard':
             print('What type of publication do you want to add? (Options: news / private ad / blog post)')
             answer = input()
             print(f'Your answer is:\n{answer}')
-            if answer.lower()=='news':
+            if answer.lower() == 'news':
                 print('What city was happened news in?')
                 city = input()
                 print('Print the text of the news')
                 text = input()
-                new_news = News(text,city)
+                # print('Input the filepath:')
+                # filepath = input()
+                # print(f'Your answer is:\n{filepath}')
+                # upload_to_print = FileUpload(filepath)
+                # upload_to_print.publishing_from_file()
+                new_news = News(text, city)
                 new_news.publishing_news()
-            elif answer.lower()=='private ad':
+            elif answer.lower() == 'private ad':
                 print('When is the deadline of this ad? (yyyy/mm/dd)')
                 deadline_date = input()
                 try:
                     expiration_date = datetime.strptime(deadline_date, '%Y/%m/%d')
                 except:
                     print('Wrong date format!\nPlease repeat the input of ad with the right date format(yyyy/mm/dd)')
-                    break
+                    break  # out of program
                 if (expiration_date - current_date).days < 0:
-                    print(f"Wrong date was input - {deadline_date}\n"
-                          f"Please repeat the input of ad with the right date (later than today)")
+                    print(
+                        f'Wrong date was input - {deadline_date}\n'
+                        f'Please repeat the input of ad with the right date (later than today)')
                     break
                 else:
                     pass
                 print(expiration_date)
                 print('Print the text of the ad')
                 text = input()
-                new_private_ad = PrivateAd(text,expiration_date)
+                new_private_ad = PrivateAd(text, expiration_date)
                 new_private_ad.publishing_ad()
-            elif answer.lower()=='private ad':
-                print('When is the deadline of this ad?')
-                expiration_date = input()
-                print('Print the text of the ad')
-                text = input()
-                current_date = date.today()
-                new_private_ad = PrivateAd(text,expiration_date)
-                new_private_ad.publishing_ad()
-            elif answer.lower()=='blog post':
+            elif answer.lower() == 'blog post':
                 print('What is a title of post?')
                 title = input()
                 print('Who is an author of this post?')
@@ -241,31 +302,53 @@ class main():
                 text = input()
                 print('What tag do you want to add to this post')
                 tag = input()
-                new_blogpost = BlogPost(text,title, author, tag)
+                new_blogpost = BlogPost(text, title, author, tag)
                 new_blogpost.publishing_post()
             else:
                 print('This type of publication can`t be added to the news feed')
 
-        elif inputway=='file':
-            print('Input the filepath:')
-            filepath = input()
-            print(f'Your answer is:\n{filepath}')
-            upload_to_print=FileUpload(filepath)
-            upload_to_print.publishing_from_file()
-            upload_to_print.delete_file()
+        elif inputway == 'file':
+            cwd = os.getcwd()
+            print(cwd)
+            print(
+                f'Provide the name of your file in the directory {cwd}'
+                f' or the filepath to the file in the another directory :')
+            fileinput = input()
+            print(f'Your answer is:\n{fileinput}')
+            inputfilepath = os.path.join(cwd, fileinput)
+            print(inputfilepath)
+            if os.path.isfile(inputfilepath):
+                filepath = inputfilepath
+                print('Exists')
+                upload_to_print = FileUpload(filepath)
+                upload_to_print.publishing_from_file()
+                upload_to_print.delete_file()
 
+            elif os.path.isfile(fileinput):
+                filepath = fileinput
+                print('Another directory')
+                upload_to_print = FileUpload(filepath)
+                upload_to_print.publishing_from_file()
+                upload_to_print.delete_file()
+            else:
+                print('Not exist')
+                break
 
         print('Do you want to add another publication? (Options: y/n)')
         another_publication = input()
-        if another_publication=='y':
+        if another_publication == 'y':
             y = 0
         else:
             y = 1
-        normalization = Normalization()
-        newsfeed = normalization.normalizing_file()
-        csv=FileCSV()
+
+        normal_file = Normalization()  # 'normalization' object is not callable error when second publication is done;
+        # use another name for variable
+
+        normal_file.normalizing_file()
+        csv = FileCSV()
         csv.count_words()
         csv.show_statistics()
 
 
-main()
+if __name__ == "__main__":
+    Main()
